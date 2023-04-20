01155995bbbcf678e45301829b3da00036c73a46
Korozin <90534409+Korozin@users.noreply.github.com>
2023-04-20 09:56:16 -0400

    Create FizzBuzz.cpp

---
+++ CLI/FizzBuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            std::cout << i << " FizzBuzz\n";
+        } else if(i % 3 == 0) {
+            std::cout << i << " Fizz\n";
+        } else if(i % 5 == 0) {
+            std::cout << i << " Buzz\n";
+        } else {
+            std::cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
